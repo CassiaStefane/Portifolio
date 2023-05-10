84510071d37d13aa7b0b5b8b541d360a0e841a4e
CassiaStefane <cassiastefani25@gmail.com>
2023-05-10 13:20:31 -0300

    is not getting better

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,12 +3,12 @@ import './Intro.css';
 
 function Header(){
     return(
-        <div class='header'>
+        <div className={'header'}>
             <div>
                 <h1>Hi Hello</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             </div>
-            <img class='foto' alt="Myself" src={foto} />
+            <img className={'foto'} alt="Myself" src={foto} />
         </div>
     )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,18 +2,18 @@ import './Project.css';
 
 function Project(){
     return(
-        <div class='conteiner'>
+        <div className={'conteiner'}>
             <h3>Projects</h3>
-            <div class='project'>
-                <div>
+            <div className={'project'}>
+                <div className={'section'}>
                     <h4>hi</h4>
                     <p>hdth nvdt yksx dxhnvd cyktdxh</p>
                 </div>
-                <div>
+                <div className={'section'}>
                     <h4>hi</h4>
                     <p>hdth nvdt yksx dxhnvd cyktdxh</p>
                 </div>
-                <div>
+                <div className={'section'}>
                     <h4>hi</h4>
                     <p>hdth nvdt yksx dxhnvd cyktdxh</p>
                 </div>
