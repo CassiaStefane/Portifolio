ca838179c8013b3e1c7acbc333b0329cc43353f0
CassiaStefane <cassiastefani25@gmail.com>
2023-05-10 10:45:54 -0300

    original idea

---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import foto from './img/foto.png';
+import './Intro.css';
+
+function Header(){
+    return(
+        <div class='header'>
+            <div>
+                <h1>Hi Hello</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
+            <img class='foto' alt="Myself" src={foto} />
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/Project.js
@@ -0,0 +1,24 @@
+import './Project.css';
+
+function Project(){
+    return(
+        <div class='conteiner'>
+            <h3>Projects</h3>
+            <div class='project'>
+                <div>
+                    <h4>hi</h4>
+                    <p>hdth nvdt yksx dxhnvd cyktdxh</p>
+                </div>
+                <div>
+                    <h4>hi</h4>
+                    <p>hdth nvdt yksx dxhnvd cyktdxh</p>
+                </div>
+                <div>
+                    <h4>hi</h4>
+                    <p>hdth nvdt yksx dxhnvd cyktdxh</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Project;
