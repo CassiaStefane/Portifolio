692f1db2e9b5670ebcb122ad2dcf3cb45c760cd3
CassiaStefane <cassiastefani25@gmail.com>
2023-07-14 16:18:40 -0300

    p2

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,6 +7,7 @@ function Header(){
             <div className={'intro'}>
                 <h1>Cássia Morais web developer</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <a href='#projetos.id'>Ver mais</a>
             </div>
             <img className={'foto'} alt="Myself" src={foto} />
         </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ function Project(){
     return(
         <div className={'conteiner'}>
             <h3>Projects</h3>
-            <div className={'project'}>
+            <div className={'project'} id={'projetos.id'}>
                 <div className={'section'}>
                     <h4>hi</h4>
                     <p>hdth nvdt yksx dxhnvd cyktdxh</p>
