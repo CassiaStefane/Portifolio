d774323dba09161dae14c36282d1941da4047bb4
CassiaStefane <cassiastefani25@gmail.com>
2023-07-21 12:25:15 -0300

    new menu

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ function Header(){
     return(
         <div className={'header'} id={'header.id'}>
             <div className={'intro'}>
-                <h1>Cássia Morais web developer</h1>
+                <h1>Meu nome aqui web developer</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
                 <a className='button' href='#projetos.id'>Ver mais</a>
             </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,18 @@ import './Nav.css';
 import { VscMenu } from "react-icons/vsc";
 
 function Nav(){
+    const handleClick = ()=>{
+        const element = document.querySelector('#display');
+        element.style.display = 'flex';}
     return(
         <div className={'Nav'}>
              <div className={"logo"}>
                 <h1>LOGO</h1>
             </div>
-            <div className={'VscMenu'} >
+            <div className={'VscMenu'} onClick={handleClick}>
                 <VscMenu/>
             </div>
-            <div className={"nav-bar"}>
+            <div className={"nav-bar"} id={'display'}>
                 <a href='#header.id'>Home</a>
                 <a href='#projetos.id'>Project</a>
                 <a href='#projetos.id'>Contact</a>
