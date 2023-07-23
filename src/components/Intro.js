e9f579e98a6732c4429d6e1ae593e7aeec769758
CassiaStefane <cassiastefani25@gmail.com>
2023-07-23 15:02:29 -0300

    new style

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,12 +1,24 @@
 import foto from './img/nana osaki icon.jpg';
 import './Intro.css';
+import { ImHtmlFive } from "react-icons/im";
+import { IoLogoJavascript } from "react-icons/io";
+import { IoLogoCss3 } from "react-icons/io";
+import { SiReact } from "react-icons/si";
 
 function Header(){
     return(
         <div className={'header'} id={'header.id'}>
             <div className={'intro'}>
-                <h1>Meu nome aqui web developer</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <h1>Meu nome aqui</h1>
+                <h3>Web developer</h3>
+                <hr></hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <div className={'icons'}>
+                <ImHtmlFive/>
+                <IoLogoCss3/>
+                <IoLogoJavascript/>
+                <SiReact/>
+                </div>
                 <a className='button' href='#projetos.id'>Ver mais</a>
             </div>
             <img className={'foto'} alt="Myself" src={foto} />
